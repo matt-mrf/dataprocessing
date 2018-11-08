767e582b3d794acc3bcd99e168eb852d5be39078
matt-mrf <mattfinn.mf@gmail.com>
2018-11-08 16:35:20 +0100

    Update homework/Week_2/eda.py

--- homework/Week_2/eda.py
+++ homework/Week_2/eda.py
@@ -5,18 +5,42 @@
 This script ...
 """
 
-import pandas
+import pandas as pd
 import csv
 
 # global constants
-INPUT_CSV = "movies.csv"
+INPUT_CSV = "input.csv"
 
 # global data_dictionary
-data_dict = {str(key): []}
+data_dict = {}
 
-print(data_dict)
 
-# def parse_data():
-#     with open(INPUT_CSV, newline='') as csvfile:
-#         reader = csv.DictReader(csvfile)
-#         for row in reader:
+def preprocess_csv():
+    """
+    Output a CSV file containing the preprocessed countries
+    """
+    with open(INPUT_CSV, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            for key in row:
+                # standardise missing data
+                if row[key] == '':
+                    row[key] = 'unknown'
+                # set GDP to numbers
+                if key == "GDP ($ per capita) dollars":
+                    row[key] = row[key].split()[0]
+
+
+            data_dict[row['Country'].strip()] = {
+                                                "Region":row['Region'].strip(),
+                                                "Population":row["Population"],
+                                                "Pop. Density (per sq. mi.)":row["Pop. Density (per sq. mi.)"],
+                                                "Infant mortality (per 1000 births)":row["Infant mortality (per 1000 births)"],
+                                                "GDP ($ per capita) dollars":row["GDP ($ per capita) dollars"],
+                                                }
+
+
+if __name__ == "__main__":
+    panda_file = pd.read_csv(INPUT_CSV)
+    print(panda_file)
+    # preprocess_csv()
