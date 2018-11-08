4b92725fa3236a8fe9ee19ca135c27f6201e1c9e
matt-mrf <mattfinn.mf@gmail.com>
2018-11-08 12:21:47 +0100

    Update homework/Week_2/eda.py

--- homework/Week_2/eda.py
+++ homework/Week_2/eda.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python
+# Name: Matthew Finnegan
+# Student number: 10698485
+"""
+This script ...
+"""
 
+import pandas
+import csv
+
+# global constants
+INPUT_CSV = "movies.csv"
+
+# global data_dictionary
+data_dict = {str(key): []}
+
+print(data_dict)
+
+# def parse_data():
+#     with open(INPUT_CSV, newline='') as csvfile:
+#         reader = csv.DictReader(csvfile)
+#         for row in reader:
