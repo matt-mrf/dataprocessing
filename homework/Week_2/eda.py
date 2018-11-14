400e2d91e44a21cbc75de6051499bb04b8536c4d
matt-mrf <mattfinn.mf@gmail.com>
2018-11-14 16:33:44 +0100

    final additions

--- homework/Week_2/eda.py
+++ homework/Week_2/eda.py
@@ -2,12 +2,14 @@
 # Name: Matthew Finnegan
 # Student number: 10698485
 """
-This script ...
+This script takes a CSV file, preprocesses it
+and analyses the data from it.
 """
 
 import pandas as pd
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 import json
 
 # global constants
@@ -18,10 +20,18 @@ NEW_CSV = "input_better.csv"
 data_dict = {}
 
 
-def preprocess_csv():
+def preprocess_data():
     """
-    Output a CSV file containing the preprocessed countries
+    Output a CSV file containing the preprocessed data
     """
+    # setting codes for strings
+    co = "Country"
+    re = "Region"
+    pop_dense = "Pop. Density (per sq. mi.)"
+    inf_mor = "Infant mortality (per 1000 births)"
+    gdp = "GDP ($ per capita) dollars"
+
+    # read the csv to be preprocessed
     with open(INPUT_CSV, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
@@ -29,97 +39,76 @@ def preprocess_csv():
                 # standardise missing data
                 if row[key] == 'unknown':
                     row[key] = ''
-                # set GDP-comlumn to numberso only
-                if key == "GDP ($ per capita) dollars":
-                    try:
-                        row[key] = row[key].split()[0]
-                    except:
-                        pass
-                elif key == "Infant mortality (per 1000 births)":
-                    try:
+                # set GDP-comlumn to numbers only
+                if key == gdp:
+                    if row[key] != '':
+                        row[key] = int(row[key].split()[0])
+                # replace all commas in infant mortality data
+                elif key == inf_mor or key == pop_dense:
+                    if row[key] != '':
                         row[key] = float(row[key].replace(',','.'))
-                    except:
-                        pass
 
-            data_dict[row['Country'].strip()] = {
-                                                "Region":row['Region'].strip(),
-                                                "Population":row["Population"],
-                                                "Pop. Density (per sq. mi.)":row["Pop. Density (per sq. mi.)"],
-                                                "Infant mortality (per 1000 births)":row["Infant mortality (per 1000 births)"],
-                                                "GDP ($ per capita) dollars":row["GDP ($ per capita) dollars"],
-                                                }
+            # put new and less data in dictionary
+            data_dict[row[co].strip()] = {
+                                         re:row[re].strip(),
+                                         pop_dense:row[pop_dense],
+                                         inf_mor:row[inf_mor],
+                                         gdp:row[gdp],
+                                         }
 
+    # write new CSV with preprocessed data
     with open(NEW_CSV, 'w', newline='') as outfile:
         writer = csv.writer(outfile)
-        writer.writerow(['Country', 'Region', 'Population', \
-        'Pop. Density (per sq. mi.)', \
-        'Infant mortality (per 1000 births)', \
-        'GDP ($ per capita) dollars'])
+        writer.writerow([co, re, pop_dense, inf_mor, gdp])
 
         for country in data_dict:
-            region = data_dict[country]["Region"]
-            population = data_dict[country]["Population"]
-            pop_density = data_dict[country]["Pop. Density (per sq. mi.)"]
-            inf_mortality = data_dict[country]["Infant mortality (per 1000 births)"]
-            gdp = data_dict[country]["GDP ($ per capita) dollars"]
+            region = data_dict[country][re]
+            pop_density = data_dict[country][pop_dense]
+            inf_mortality = data_dict[country][inf_mor]
+            gdp_value = data_dict[country][gdp]
 
-            writer.writerow([country, region, population, pop_density, \
-                            inf_mortality, gdp])
+            writer.writerow([country, region, pop_density, \
+                            inf_mortality, gdp_value])
 
-def plot_hist(pandas_df, summary):
+
+def plot_graph(gdp_values ,summary):
     """
-    Plots a histogram of the GDP in a country
+    PLots charts of the GDP values and of
+    the descriptive values of infant mortality
     """
-    f = plt.figure(1)
-    y = list(pandas_df["GDP ($ per capita) dollars"])
+    # set the y values for the figures
+    plt1_x = gdp_values
+    plt2_y = summary
 
+    # make figure 1
+    plt.figure(1)
     plt.tick_params(
-    axis='x',          # changes apply to the x-axis
-    which='both',      # both major and minor ticks are affected
-    bottom=False,      # ticks along the bottom edge are off
-    top=False,         # ticks along the top edge are off
-    labelbottom=False) # labels along the bottom edge are off
+        axis='x',
+        which='both',
+        bottom=True,
+        labelbottom=True
+        )
+    # set labels and plot
+    plt.title('GDP frequency over countries')
+    plt.xlabel('GDP')
+    plt.ylabel('Frequency')
+    plt.hist(plt1_x)
 
-    # plot and set limits
-    plt.plot(y)
+    # make figure 2
+    plt.figure(2)
+    plt.tick_params(
+        axis='x',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        bottom=False,      # ticks along the bottom edge are off
+        labelbottom=False  # labels along the bottom edge are off
+        )
 
-    # set labels
-    plt.title('GDP per country')
-    plt.ylabel('GDP')
-
-    f.show()
-
-    x = list(summary.keys())
-    y2 = list(summary.values())
-
-    g = plt.figure(2)
-    plt.bar(x, y2)
-
-    # set labels
+    # set labels and plot
     plt.title('Summary of infant mortality')
-    plt.xlabel('')
-    plt.ylabel('')
-
-    g.show()
-
-    input()
-
-
-def plot_bar(summary):
-    """
-    PLots a bar chart of the descriptive values of infant mortality
-    """
-    x = list(summary.keys())
-    y = list(summary.values())
-
-    plt.bar(x, y)
-
-    # set labels
-    plt.title('Summary of infant mortality')
-    plt.xlabel('')
-    plt.ylabel('')
+    plt.boxplot(plt2_y, whis=100)
 
     plt.show()
+
 
 def to_json():
     with open('result.json', 'w') as fp:
@@ -127,28 +116,43 @@ def to_json():
 
 
 if __name__ == "__main__":
-    preprocess_csv()
+    preprocess_data()
 
+    # put data into dataframe
     pandas_file = pd.read_csv(NEW_CSV)
-    pandas_df = pd.DataFrame(pandas_file)
+    df = pd.DataFrame(pandas_file)
 
-    gdp_median = int(pandas_df["GDP ($ per capita) dollars"].median())
-    gdp_mean = int(pandas_df["GDP ($ per capita) dollars"].mean())
-    gdp_mode = int(pandas_df["GDP ($ per capita) dollars"].mode())
-    gdp_std = pandas_df["GDP ($ per capita) dollars"].std()
+    # take GDP column
+    target = df['GDP ($ per capita) dollars']
 
-    inf_des = pandas_df["Infant mortality (per 1000 births)"].describe()
-    inf_med = pandas_df["Infant mortality (per 1000 births)"].median()
-    summary = {"Minimum":inf_des["min"], \
-               "First Quartile":inf_des["25%"], \
-               "Median":inf_med, \
-               "Third Quartile":inf_des["75%"], \
-               "Maximum":inf_des["max"]}
+    # filtering out outliers that are removed more than
+    # 2 times the standard deviation
+    gdp_mean = int(target.mean())
+    gdp_sd = int(target.std())
+    selected_df = df[(target > gdp_mean - 2*gdp_sd) & (target < gdp_mean + 2*gdp_sd)]
 
-    plot_hist(pandas_df, summary)
-    # plot_bar(summary)
+    target2 = selected_df['GDP ($ per capita) dollars']
+
+    # calculating the new mean, median, mode and standard deviation
+    gdp_mean = int(target2.mean())
+    gdp_median = int(target2.median())
+    gdp_mode = int(target2.mode())
+    gdp_sd = int(target2.std())
+    print(f"GDP data: median: {gdp_median}, mean: {gdp_mean}, "+ \
+    f"mode: {gdp_mode}, std. dev: {gdp_sd}")
+
+    gdp_values = list(target2)
+
+    inf_des = df["Infant mortality (per 1000 births)"].describe()
+
+    min = inf_des["min"]
+    first_q = inf_des["25%"]
+    median = inf_des["50%"]
+    third_q = inf_des["75%"]
+    max = inf_des["max"]
+
+    summary = [min, first_q, median, third_q, max]
+
+    plot_graph(gdp_values, summary)
 
     to_json()
-
-    print(f"median: {gdp_median}, mean: {gdp_mean}, mode: {gdp_mode}, "+ \
-         f"std. dev: {gdp_std}")
