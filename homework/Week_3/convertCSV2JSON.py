7e6a5893c1446b189f527f86a569eaee0ddad70f
matt-mrf <mattfinn.mf@gmail.com>
2018-11-15 15:07:57 +0100

    convert to json

---
+++ homework/Week_3/convertCSV2JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# Name: Matthew Finnegan
+# Student number: 10698485
+"""
+This script converts a csv file with
+Toyota import numbers in Norway to json
+"""
+
+import json
+import csv
+
+INPUT_CSV = 'norway_new_car_sales_by_make.csv'
+OUTPUT_JSON = 'norway_cars.json'
+
+data = {}
+with open(INPUT_CSV) as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        if row["Make"] == "Toyota":
+            if row["Year"] in data:
+                data[row["Year"]] += int(row["Quantity"])
+            else:
+                data[row["Year"]] = int(row["Quantity"])
+
+with open(OUTPUT_JSON, 'w') as fp:
+        json.dump(data, fp)
