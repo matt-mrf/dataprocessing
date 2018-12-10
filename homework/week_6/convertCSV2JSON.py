039593ccbc69d499428866e37a7aaa1833f4978b
matt-mrf <mattfinn.mf@gmail.com>
2018-12-10 12:55:57 +0100

    init week 6

--- homework/week_6/convertCSV2JSON.py
+++ homework/week_6/convertCSV2JSON.py
@@ -5,15 +5,16 @@
 This script converts a csv file with
 Toyota import numbers in Norway to json
 
-[
+{
    2008: {
             Rav4: 206694
             Avensis: 35067
             total:
          }
-]
+}
 """
 
+import pprint
 import json
 import csv
 
@@ -25,11 +26,23 @@ with open(INPUT_CSV) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         if row["Make"].strip() == "Toyota":
-            print(row)
-            # if row["Year"] in data:
-            #     data[row["Year"]] += int(row["Quantity"])
-            # else:
-            #     data[row["Year"]] = int(row["Quantity"])
+            if row["Year"] in data:
+                if row["Model"] in data[row["Year"]]:
+                    data[row["Year"]][row["Model"]] += int(row["Quantity"])
+                    data[row["Year"]]["total"] += int(row["Quantity"])
+                else:
+                    data[row["Year"]][row["Model"]] = int(row["Quantity"])
+                    data[row["Year"]]["total"] += int(row["Quantity"])
+            else:
+                data[row["Year"]] = {row["Model"]: int(row["Quantity"]),
+                                     "total": int(row["Quantity"])}
+
+data_list = []
+for k, v in data.items():
+    temp_obj = {}
+    temp_obj["year"] = k
+    temp_obj["models"] = v
+    data_list.append(temp_obj)
 
 with open(OUTPUT_JSON, 'w') as fp:
-    json.dump(data, fp)
+    json.dump(data_list, fp)
